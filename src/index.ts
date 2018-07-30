1ff0038b7a494673df69df6a4fc251f716c1dacb
Saban Uenlue - netTrek <us@netTrek.de>
2018-07-30 12:06:02 +0200

    clean

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,2 @@
-import { Car } from './product/Car';
-
 export const elem = document.querySelector( 'h1' );
- elem.innerText = 'Hello TypeScript !!!!';
-
- console.log ( elem , 11 );
-
-const c = new Car();
-console.log ( c );
+elem.innerText = 'Hello TypeScript !!!!';
--- src/product/Car.ts
+++
@@ -1,9 +0,0 @@
-/**
- * File created by suenlue on 29.07.18.
- * Copyright (c) 2018 by netTrek GmbH & Co. KG
- */
-
-export class Car {
-  type = 'saban';
-  constructor () {}
-}
